df70c8c3a72308d1f3759fafe693266e380cd330
Miriam-Janet <43157976+Miriam-Janet@users.noreply.github.com>
2022-03-02 09:44:24 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Otro cambio nuevo");
+		get("/hello", (req, res) -> "Cambio con jenkis con un webhook");
 	}
 }
